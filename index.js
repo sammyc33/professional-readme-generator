5a39aaa876e2b1094d29df5a8ece6c704e4ff116
Sammy <sammy.campbell33@gmail.com>
2022-10-18 19:35:42 -0700

    Update question array to inquirer format

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
+//const questions = [
+inquirer
+    .prompt([
     {
         type: "input",
         message: "What is the title of your project?",
@@ -60,15 +62,13 @@ const questions = [
         choices: ['MIT', 'Apache', 'BSD', 'GPL', 'Mozilla-Public', 'None'],
         name: "projLicense"
     },
-];
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    
-};
+//function writeToFile(fileName, data) {};
 
 // TODO: Create a function to initialize app
-function init() 
-
+//function init() => { 
+   
 // Function call to initialize app
-init();
+//init();
